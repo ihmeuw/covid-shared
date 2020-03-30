b21b53ff33b7345d0ade737949b350e587932f71
James Collins <collijk@uw.edu>
2020-03-29 18:39:49 -0700

    Production update

--- src/covid_input_shared/__about__.py
+++ src/covid_input_shared/__about__.py
@@ -6,7 +6,7 @@ __title__ = "covid-input-shared"
 __summary__ = "Shared utilities for the covid model input pipeline."
 __uri__ = "https://stash.ihme.washington.edu/projects/CVD19/repos/covid-input-shared/browse"
 
-__version__ = 'alpha'
+__version__ = 'd_2020_03_29_t_18_35'
 
 __author__ = "James Collins"
 __email__ = "collijk@uw.edu"
