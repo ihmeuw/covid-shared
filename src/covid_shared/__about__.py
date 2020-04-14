6da4d9adf33a0a8e12cad77ec57284a1677f60df
James Collins <collijk@uw.edu>
2020-04-14 11:54:37 -0700

    switch to semver for deployment

--- src/covid_shared/__about__.py
+++ src/covid_shared/__about__.py
@@ -6,7 +6,7 @@ __title__ = "covid-shared"
 __summary__ = "Shared utilities for the covid model pipeline."
 __uri__ = "https://stash.ihme.washington.edu/projects/CVD19/repos/covid-shared/browse"
 
-__version__ = 'd_2020_03_29_t_18_35'
+__version__ = '1.0.0'
 
 __author__ = "James Collins"
 __email__ = "collijk@uw.edu"
