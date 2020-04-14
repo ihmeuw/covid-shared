e292ab7cb379488291180783b769da5fdfd2a92d
James Collins <collijk@uw.edu>
2020-04-14 11:50:38 -0700

    Update to rst in setup as well

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     with open(os.path.join(src_dir, "covid_shared", "__about__.py")) as f:
         exec(f.read(), about)
 
-    with open(os.path.join(base_dir, "README.md")) as f:
+    with open(os.path.join(base_dir, "README.rst")) as f:
         long_description = f.read()
 
     install_requirements = [
