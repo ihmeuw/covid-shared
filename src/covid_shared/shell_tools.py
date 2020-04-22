f0b897301a0af2f4fd8f5fd217a99129d08cfdba
Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
2020-04-22 12:00:39 -0700

    create mkdir utility function that sets permissions correctly

--- src/covid_shared/shell_tools.py
+++ src/covid_shared/shell_tools.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 import shlex
 import subprocess
@@ -31,3 +32,19 @@ def unzip_and_delete_archive(archive_path: Union[str, Path], output_path: Union[
     """
     subprocess.run(shlex.split(f'unzip {archive_path} -d {output_path}'), check=True)
     subprocess.run(shlex.split(f'rm {archive_path}'), check=True)
+
+
+def mkdir(path: Union[str, Path], umask: int = 0o002, exists_ok: bool = False, parents: bool = False) -> None:
+    """
+    Utility method to create a directory with specified permissions
+    :param path: path of the directory to create
+    :param umask: umask specifying the desired permissions - defaults to 0o002
+    :param exists_ok: if False, raises FileExistsError if the directory already exists
+    :param parents: if False, raises FileNotFoundError if the directory's parent doesn't exist
+    """
+    path = Path(path)
+    old_umask = os.umask(umask)
+    try:
+        path.mkdir(exist_ok=exists_ok, parents=parents)
+    finally:
+        os.umask(old_umask)
