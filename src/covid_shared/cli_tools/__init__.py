b8d2bd9654f38e9d83ef7c0219b01b836088b019
James Collins <collijk@uw.edu>
2021-01-16 20:37:05 -0800

    Split off verbose, with_debugger, output_root, cleanup imports

--- src/covid_shared/cli_tools/__init__.py
+++ src/covid_shared/cli_tools/__init__.py
@@ -1,15 +1,49 @@
-from covid_shared.cli_tools.decorators import (add_verbose_and_with_debugger, pass_run_metadata,
-                                               with_mark_best, with_production_tag,
-                                               add_output_options, add_r_singularity_option,
-                                               add_mobility_gpr_dependency_option, add_model_inputs_dependency_option,
-                                               add_seir_covariates_dependency_option, add_snapshot_dependency_option)
-from covid_shared.cli_tools.logging import (LOG_FORMATS, DEFAULT_LOG_MESSAGING_FORMAT, add_logging_sink,
-                                            configure_logging_to_files, configure_logging_to_terminal)
-from covid_shared.cli_tools.metadata import (Metadata, RunMetadata, YamlIOMixin, get_function_full_argument_mapping,
-                                             monitor_application, update_with_previous_metadata)
-from covid_shared.cli_tools.run_directory import (get_last_stage_directory, get_current_previous_version,
-                                                  get_run_directory, make_links, make_run_directory, mark_best,
-                                                  mark_best_explicit, mark_explicit, mark_latest, mark_latest_explicit,
-                                                  mark_production, mark_production_explicit, move_link,
-                                                  setup_directory_structure)
-from covid_shared.cli_tools.cleanup import finish_application
+from covid_shared.cli_tools.decorators import (
+    add_verbose,
+    add_with_debugger,
+    add_verbose_and_with_debugger,
+    pass_run_metadata,
+    with_output_root,
+    with_mark_best,
+    with_production_tag,
+    add_output_options,
+    add_r_singularity_option,
+    add_mobility_gpr_dependency_option,
+    add_model_inputs_dependency_option,
+    add_seir_covariates_dependency_option,
+    add_snapshot_dependency_option
+)
+from covid_shared.cli_tools.logging import (
+    LOG_FORMATS,
+    DEFAULT_LOG_MESSAGING_FORMAT,
+    add_logging_sink,
+    configure_logging_to_files,
+    configure_logging_to_terminal
+)
+from covid_shared.cli_tools.metadata import (
+    Metadata,
+    RunMetadata,
+    YamlIOMixin,
+    get_function_full_argument_mapping,
+    monitor_application,
+    update_with_previous_metadata
+)
+from covid_shared.cli_tools.run_directory import (
+    get_last_stage_directory,
+    get_current_previous_version,
+    get_run_directory,
+    make_links,
+    make_run_directory,
+    mark_best,
+    mark_best_explicit,
+    mark_explicit,
+    mark_latest,
+    mark_latest_explicit,
+    mark_production,
+    mark_production_explicit,
+    move_link,
+    setup_directory_structure
+)
+from covid_shared.cli_tools.cleanup import (
+    finish_application,
+)
--- src/covid_shared/cli_tools/decorators.py
+++ src/covid_shared/cli_tools/decorators.py
@@ -8,22 +8,65 @@ from covid_shared.cli_tools.metadata import RunMetadata, get_function_full_argum
 from covid_shared.paths import BEST_LINK, R_SINGULARITY_IMAGE_PATH
 
 
-# Common click options
-logging_options = [
-    click.option('--pdb', 'with_debugger',
-                 is_flag=True,
-                 help='Drop into python debugger if an error occurs.'),
-    click.option('-v', 'verbose',
-                 count=True,
-                 help='Configure logging verbosity.'),
-]
+add_verbose = click.option(
+    '-v', 'verbose',
+    count=True,
+    help='Configure logging verbosity.'
+)
+add_with_debugger = click.option(
+    '--pdb', 'with_debugger',
+    is_flag=True,
+    help='Drop into python debugger if an error occurs.'
+)
 
 
 def add_verbose_and_with_debugger(func: types.FunctionType):
-    """Add all the shared options to the command."""
-    for option in logging_options:
-        func = option(func)
+    """Add both verbose and with debugger options."""
+    func = add_verbose(func)
+    func = add_with_debugger(func)
     return func
+
+
+with_production_tag = click.option(
+    '-p', '--production-tag',
+    type=click.STRING,
+    help='Tags this run as a production run.'
+)
+with_mark_best = click.option(
+    '-b', '--mark-best',
+    is_flag=True,
+    help='Mark this run as "best"'
+)
+
+
+def with_output_root(default_output_root: Path):
+    """Adds output root option with a default."""
+
+    def wrapper(entry_point: types.FunctionType):
+        entry_point = click.option(
+            "-o", "--output-root",
+            type=click.Path(file_okay=False),
+            default=default_output_root,
+            help=(f"Directory to outputs. Defaults to {default_output_root}/YYYY_MM_DD.VV for today's date and the "
+                  "newest uncreated version")
+        )(entry_point)
+        return entry_point
+
+    return wrapper
+
+
+def add_output_options(default_output_root: Path):
+    """
+    Decorator to set CLI command options required when producing outputs.
+    """
+
+    def wrapper(entry_point: types.FunctionType):
+        entry_point = with_output_root(default_output_root)(entry_point)
+        entry_point = with_production_tag(entry_point)
+        entry_point = with_mark_best(entry_point)
+        return entry_point
+
+    return wrapper
 
 
 def pass_run_metadata(run_metadata: RunMetadata = RunMetadata()):
@@ -64,39 +107,6 @@ def add_r_singularity_option():
             help=(f"Path to a specific R singularity. Can pass 'None' to use active conda environment. "
                   f"By default uses {R_SINGULARITY_IMAGE_PATH}.")
         )(entry_point)
-
-        return entry_point
-    return wrapper
-
-
-with_production_tag = click.option(
-    '-p', '--production-tag',
-    type=click.STRING,
-    help='Tags this run as a production run.'
-)
-with_mark_best = click.option(
-    '-b', '--mark-best',
-    is_flag=True,
-    help='Mark this run as "best"'
-)
-
-def add_output_options(default_output_root: Path):
-    """
-    Decorator to set CLI command options required when producing outputs.
-    """
-    def wrapper(entry_point: types.FunctionType):
-
-        entry_point = click.option(
-            "-o", "--output-root",
-            type=click.Path(file_okay=False),
-            default=default_output_root,
-            help=(f"Directory to outputs. Defaults to {default_output_root}/YYYY_MM_DD.VV for today's date and the "
-                  "newest uncreated version")
-        )(entry_point)
-
-        entry_point = with_production_tag(entry_point)
-
-        entry_point = with_mark_best(entry_point)
 
         return entry_point
     return wrapper
