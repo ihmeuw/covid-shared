163293f300d1d04451e27f7dbb878ab6089f286c
Steve Bachmeier <sbachmei@uw.edu>
2021-03-08 10:33:07 -0800

    Remove unnecessary bool()s

--- src/covid_shared/cli_tools/decorators.py
+++ src/covid_shared/cli_tools/decorators.py
@@ -175,5 +175,5 @@ def validate_options_with_q(quick: int, mark_best: bool, production_tag: str):
     """
     Quick runs should never be marked 'best' or for production
     """
-    if (bool(quick)) and (mark_best or bool(production_tag)):
+    if quick and (mark_best or production_tag):
         raise ValueError("Cannot mark a quick snapshot as best or for production.")
