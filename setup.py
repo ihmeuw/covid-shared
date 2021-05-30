a1a8b25225150e33d86c3c46ad9fa627153642ed
James Collins <collijk@uw.edu>
2021-05-30 09:27:29 -0700

    Add internal reqs

--- setup.py
+++ setup.py
@@ -51,7 +51,8 @@ if __name__ == "__main__":
         install_requires=install_requirements,
         extras_require={
             'test': test_requirements,
-            'dev': test_requirements + doc_requirements,
+            'internal': internal_requirements,
+            'dev': test_requirements + doc_requirements + internal_requirements,
             'docs': doc_requirements
         },
 
