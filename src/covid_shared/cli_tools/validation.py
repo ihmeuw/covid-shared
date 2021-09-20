2e5834d1af2dad69792edd9e5908e3cfe27f500a
Steve Bachmeier <sbachmei@uw.edu>
2021-09-20 11:54:08 -0700

    change 'snapshot' to 'run'

--- src/covid_shared/cli_tools/validation.py
+++ src/covid_shared/cli_tools/validation.py
@@ -3,4 +3,4 @@ def validate_best_and_production_tags(modifier: bool, mark_best: bool, productio
     Incomplete runs should never be marked 'best' or for production, eg quick, trial-run, etc
     """
     if modifier and (mark_best or production_tag):
-        raise ValueError("Cannot mark an incomplete snapshot as best or for production.")
+        raise ValueError("Cannot mark an incomplete run as best or for production.")
