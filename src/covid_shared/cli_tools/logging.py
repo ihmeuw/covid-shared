f80ce51666c7b20d7a7c8276f2eba71d61d367a6
James Collins <collijk@uw.edu>
2022-02-21 19:06:09 -0800

    A lot of fiddling with jobmon logging

--- src/covid_shared/cli_tools/logging.py
+++ src/covid_shared/cli_tools/logging.py
@@ -10,7 +10,13 @@ from covid_shared import paths
 from covid_shared.shell_tools import mkdir
 
 
-class InterceptHandler(logging.Handler):
+class JobmonInterceptHandler(logging.Handler):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._queued = 0
+        self._running = 0
+
     def emit(self, record):
         try:
             level = logger.level(record.levelname).name
@@ -22,9 +28,48 @@ class InterceptHandler(logging.Handler):
             frame = frame.f_back
             depth += 1
 
-        logger.opt(depth=depth, exception=record.exc_info).log(
-            level, record.getMessage()
+        message, log_from_here = self.parse_record(record)
+        if message is None:
+            return
+        elif log_from_here:
+            logger.info(message)
+        else:
+            logger.opt(depth=depth, exception=record.exc_info).log(
+                level, message,
+            )
+
+    def parse_record(self, record):
+        log_from_here = False
+        if record.levelno > logging.INFO:
+            return record.getMessage(), log_from_here
+        drop = (
+            (
+                record.funcName in ['_send_request', 'is_5XX']
+            ) or (
+                record.name == 'jobmon.client.workflow'
+                and record.funcName in ['add_task', '_distributor_alive']
+            ) or (
+                record.name == 'jobmon.client.distributor.distributor_service'
+                and record.funcName in ['_create_task_instance', '_keep_distributing', 'heartbeat', '_get_lost_task_instances', '_distribute_forever']
+            )
         )
+        if drop:
+            msg = None
+        elif record.name == 'jobmon.client.distributor.distributor_service':
+            msg = record.getMessage()
+            if 'active distributor_ids:' in msg:
+                self._running = str(len(msg.split(',')) - 1)
+                msg = f'Queued: {self._queued}, Running: {self._running}'
+                log_from_here = True
+            elif 'Queued' in msg:
+                self._queued = msg.split(' ')[1]
+                msg = f'Queued: {self._queued}, Running: {self._running}'
+                log_from_here = True
+            else:
+                msg = None
+        else:
+            msg = record.getMessage()
+        return msg, log_from_here
 
 
 DEFAULT_LOG_MESSAGING_FORMAT = ('<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | '
@@ -33,22 +78,43 @@ DEFAULT_LOG_MESSAGING_FORMAT = ('<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> |
 
 LOG_FORMATS = {
     # Keys are verbosity.  Specify special log formats here.
-    0: ("ERROR", DEFAULT_LOG_MESSAGING_FORMAT),
+    0: ("WARNING", DEFAULT_LOG_MESSAGING_FORMAT),
     1: ("INFO", DEFAULT_LOG_MESSAGING_FORMAT),
     2: ("DEBUG", DEFAULT_LOG_MESSAGING_FORMAT),
 }
+
+def listloggers():
+    rootlogger = logging.getLogger()
+    print(rootlogger)
+    for h in rootlogger.handlers:
+        print('     %s' % h)
+
+    import pdb; pdb.set_trace()
+    for nm, lgr in logging.Logger.manager.loggerDict.items():
+        print('+ [%-20s] %s ' % (nm, lgr))
+        if not isinstance(lgr, logging.PlaceHolder):
+            for h in lgr.handlers:
+                print('     %s' % h)
+
+
+def intercept_jobmon_logs(level):
+    handler = JobmonInterceptHandler(level)
+    for name, logger in logging.Logger.manager.loggerDict.items():
+        if 'jobmon' in name and not isinstance(logger, logging.PlaceHolder):
+            logger.handlers = [handler]
 
 
 def add_logging_sink(sink: Union[typing.TextIO, Path], verbose: int, colorize: bool = False, serialize: bool = False):
     """Add a new output file handle for logging."""
     level, message_format = LOG_FORMATS.get(verbose, LOG_FORMATS[max(LOG_FORMATS.keys())])
-    logger.add(sink, colorize=colorize, level=level, format=message_format, serialize=serialize)
+    logger.add(sink, colorize=colorize, level=level, format=message_format, serialize=serialize, filter={'urllib3': 'WARNING'})
 
 
 def configure_logging_to_terminal(verbose: int):
     """Setup logging to sys.stdout."""
-    logger.remove(0)  # Clear default configuration
-    logging.basicConfig(handlers=[InterceptHandler()], level=0)
+    logger.remove()  # Clear default configuration
+    level = LOG_FORMATS.get(verbose, LOG_FORMATS[max(LOG_FORMATS.keys())])[0]
+    intercept_jobmon_logs(level)
     add_logging_sink(sys.stdout, verbose, colorize=True)
 
 
